1b3e90a48811f5f8412bc59dc3231079bf5a2e72
Zymass <filyaevilya21@gmail.com>
2021-09-29 18:03:37 +0300

    Initial Commit

---
+++ NewAppCollageLounge/NewAppCollageLoungeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewAppCollageLoungeApp.swift
+//  NewAppCollageLounge
+//
+//  Created by Feduk on 29.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct NewAppCollageLoungeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
