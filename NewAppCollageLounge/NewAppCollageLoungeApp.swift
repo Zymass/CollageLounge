b100aa5b510fd07f4db9e836917b9205da349dc5
Zymass <70097857+Zymass@users.noreply.github.com>
2021-10-13 16:22:19 +0300

    new feature

--- NewAppCollageLounge/NewAppCollageLoungeApp.swift
+++ NewAppCollageLounge/NewAppCollageLoungeApp.swift
@@ -6,10 +6,11 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct NewAppCollageLoungeApp: App {
-//    @UIApplicationDelegateAdaptor(Delegate.self) var delegate
+    @UIApplicationDelegateAdaptor(Delegate.self) var delegate
 
     var body: some Scene {
         WindowGroup {
@@ -18,10 +19,11 @@ struct NewAppCollageLoungeApp: App {
     }
 }
 
-//class Delegate : NSObject,UIApplicationDelegate{
-//
-//    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-//
-//        return true
-//    }
-//}
+class Delegate : NSObject,UIApplicationDelegate{
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+
+        FirebaseApp.configure()
+        return true
+    }
+}
